60ab0663c7d1473027976fc38ba6589f4f3a36fe
Sadeq <sadeq.gholami2014@gmail.com>
2019-04-24 19:55:24 +0200

    construstor to Amount

--- POS/src/DTOs/Amount.java
+++ POS/src/DTOs/Amount.java
@@ -1,5 +1,10 @@
 package DTOs;
 
 public class Amount {
-
+	private String currency;
+	private double amount;
+	public Amount (String currency, double amount) {
+		this.currency = currency;
+		this.amount =  amount;
+	}
 }
