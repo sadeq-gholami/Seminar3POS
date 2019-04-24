66ee8c97016c785cbd2d540d99cf2497c616312c
Sadeq <sadeq.gholami2014@gmail.com>
2019-04-24 19:53:14 +0200

    added class Amount.java and updated SaleDTO

--- POS/src/DTOs/SaleDTO.java
+++ POS/src/DTOs/SaleDTO.java
@@ -1,5 +1,12 @@
 package DTOs;
 //this is a place holder for saleinfo
 public class SaleDTO {
+	private Amount amount;
+	private String dateAndTime;
+	private String message;
+	private Amount change; 
+	
+	
+	
 
 }
