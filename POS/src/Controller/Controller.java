7eec4b8edc448136cdbc52090f4b24f8e92895be
mqdsh <mqdsh@130.229.171.12>
2019-04-25 11:08:26 +0200

    jag gjorde

--- POS/src/Controller/Controller.java
+++ POS/src/Controller/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
 	    private Sale sale;
 	    
 	    public Controller() {
-	      kkkkkkkkkkkkkkkhhhhhhhhhhhhhhhhhhhhhhhh  
+	     // kkkkkkkkkkkkkkkhhhhhhhhhhhhhhhhhhhhhhhh  
 	    }
 	    
 	    public void startNewSale() {
