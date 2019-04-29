c82bbdc967b3110c360e26e52113614c9bedb47d
Fariba Yazdani <fariba.yazdani11@gmail.com>
2019-04-29 23:25:13 +0200

    adding unit test for Receipt class

---
+++ POS/src/testPackage/ReceiptTest.java
@@ -0,0 +1,58 @@
+package testPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import se.kth.iv1500.POS.DTOs.Amount;
+import se.kth.iv1500.POS.DTOs.ItemDTO;
+import se.kth.iv1500.POS.DTOs.SaleDTO;
+import se.kth.iv1500.POS.model.Receipt;
+
+public class ReceiptTest {
+	
+	Receipt receipt;
+	
+	
+	@Before
+	public void before() {
+		Amount amount = new Amount(11,"KR");
+		List<ItemDTO> itemInfo = new ArrayList();
+		Amount change = new Amount(1,"KR");
+		
+		SaleDTO saleDTO = new SaleDTO(amount,itemInfo,change);
+		
+		receipt = new Receipt(saleDTO);
+		
+		
+	}
+	
+	@Test
+	public void testCreateReceiptString() {
+		
+		String receiptString = receipt.createReceiptString();
+		
+		Assertions.assertTrue(
+			receiptString.startsWith("Sale Receipt"));
+				
+		Assertions.assertTrue(
+				receiptString.contains("Total price: 11 KR"));		
+					
+		Assertions.assertTrue(
+				receiptString.contains("Change: 1 KR"));		
+		
+		Assertions.assertTrue(
+				receiptString.contains("Sale time: "));
+		
+	}
+
+
+}
