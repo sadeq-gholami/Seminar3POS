a61374b87007163803c87219db89b17926677ef1
Sadeq <sadeq.gholami2014@gmail.com>
2019-04-30 13:21:33 +0200

    test classes

--- POS/src/testPackage/ReceiptTest.java
+++ POS/src/testPackage/ReceiptTest.java
@@ -27,8 +27,9 @@ public class ReceiptTest {
 		Amount amount = new Amount(11,"KR");
 		List<ItemDTO> itemInfo = new ArrayList();
 		Amount change = new Amount(1,"KR");
+		Amount runningTotal = new Amount(100, "kr");
 		
-		SaleDTO saleDTO = new SaleDTO(amount,itemInfo,change);
+		SaleDTO saleDTO = new SaleDTO(amount,itemInfo,change, runningTotal);
 		
 		receipt = new Receipt(saleDTO);
 		
